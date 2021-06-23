8987a2cb2afb33bc4784f370ccd41bb24f6a6086
Nataniel Santos <nathan.santos159@hotmail.com>
2021-06-23 20:50:52 -0300

    exercicios javascript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercise_1.js
@@ -0,0 +1,16 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+let a = 13;
+let b = 39;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multipicao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multipicao);
+console.log(divisao);
+console.log(modulo);
