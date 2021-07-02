f82413093dba30e52e8ca5fd3db0096350e305d7
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-02 16:38:45 -0300

    exercicio bonus 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-bonus.js
@@ -59,3 +59,22 @@ function convertRomanAlgarism(string) {
   return sum;
 }
 console.log(convertRomanAlgarism("DLV"));
+
+
+// 2 Crie uma função chamada arrayOfNumbers que receberá a variável vector como parâmetro. Através de um loop for , percorra essa variável, busque os números pares e os adicione a um novo array que deverá ser retornado ao final pela pela função.
+
+let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+
+function getNumerosPares(vetor){
+  let arrayOfPares = []
+  for (let array of vetor){
+    for (let number of array){
+      if (number % 2 === 0){
+        arrayOfPares.push(number)
+      }
+    }
+  }
+
+  return arrayOfPares
+}
+console.log(getNumerosPares(vector))
