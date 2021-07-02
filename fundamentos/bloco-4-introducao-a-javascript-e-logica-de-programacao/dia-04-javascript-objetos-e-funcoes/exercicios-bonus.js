d68b88d68bc3cae49f03b92c398ecba99f5b3f11
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-02 17:34:19 -0300

    exercicio 3 bonus

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-bonus.js
@@ -78,3 +78,53 @@ function getNumerosPares(vetor){
   return arrayOfPares
 }
 console.log(getNumerosPares(vector))
+
+// 3 A partir do array de frutas basket , retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor. Por exemplo, o array ['Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva'], deverá retornar { Melancia: 3, Abacate: 1, Uva: 1 }
+//  quando passado como argumento para a função. Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
+
+
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+function countFruits(basket) {
+  let arrayOfFruits = []
+
+  for (let fruit of basket) {
+    let currentFruitCount = 0;
+
+    for (j=0; j < basket.length; j++){
+      if (fruit === basket[j]){
+        currentFruitCount++
+        basket.splice(j, 1)
+        j--;
+      }
+    }
+
+    arrayOfFruits.push({[fruit]: currentFruitCount});
+  }
+  return arrayOfFruits
+}
+let fruitsObjects = (countFruits(basket))
+console.log( "Sua cesta possui: ")
+for(let fruit of fruitsObjects){
+  for (fruitname in fruit ){
+    console.log(fruit[fruitname], fruitname + "s")
+  }
+}
