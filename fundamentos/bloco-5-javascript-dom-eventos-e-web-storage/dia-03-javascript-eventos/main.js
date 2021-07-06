2dca2b47d525ccb4930a09d667532eabb17be034
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-06 20:47:07 -0300

    exercicio 4

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
@@ -52,6 +52,9 @@ function changePlaceholder(element) {
 // 4 Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1 Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', function() {
+  windows.location.replace("https://natanielsantos159.github.io/")
+})
 // 5 Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
