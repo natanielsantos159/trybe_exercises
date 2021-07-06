a4d87d07f6acc77ac6d915ae6ffa40a46920310e
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-06 20:33:37 -0300

    exercicio 3 + refatoraçao do exercicio 2, criacao de classe pra remocao de classe tech e alteraçao do texto do placeholder

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
@@ -8,21 +8,44 @@ const myWebpage = document.getElementById("my-spotrybefy");
  1 Copie esse arquivo e edite apenas ele;
  */
 
-
 // 2 Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 // 2.1 Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-function addClassTech(element){
-  let techClasses = document.getElementsByClassName('tech')
-  if(techClasses.length > 0){
-    for (let classes of techClasses){
-      classes.className = '';
+function addClassTech(element) {
+  let techClasses = document.getElementsByClassName("tech");
+  if (techClasses.length > 0) {
+    for (let classes of techClasses) {
+      removerClassTech(classes)
     }
   }
-  element.className = "tech"
+  element.className = "tech";
 }
-
+function removerClassTech(element) {
+  element.className = "";
+}
 // 3 Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+input.addEventListener("keypress", getInput);
+
+function getInput(element) {
+  let classTech = document.getElementsByClassName("tech")[0];
+  if (element.key === "Enter") {
+    /* Referencia: https://stackoverflow.com/questions/14542062/eventlistener-enter-key */
+    classTech.innerText = element.target.value;
+    element.target.value = "";
+
+    if (element.target.placeholder.includes("primeira")) {
+      element.target.placeholder = element.target.placeholder.replace("primeira", "segunda");
+      addClassTech(secondDiv)
+    } else if (element.target.placeholder.includes("segunda")) {
+      element.target.placeholder = element.target.placeholder.replace("segunda", "terceira");
+      addClassTech(thirdDiv)
+    } else if (element.target.placeholder.includes("terceira")) {
+      element.target.placeholder = element.target.placeholder.replace("terceira", "primeira");
+      removerClassTech(thirdDiv)
+    }
+  }
+}
 // 4 Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1 Que tal redirecionar para seu portifólio?
