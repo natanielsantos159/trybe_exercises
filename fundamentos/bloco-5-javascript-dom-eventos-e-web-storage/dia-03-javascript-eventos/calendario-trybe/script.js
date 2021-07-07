dfd7e26039ff88cc0b45b99490e8bf7ea28a1960
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-07 12:50:49 -0300

    exercicio 7

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/calendario-trybe/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/calendario-trybe/script.js
@@ -134,3 +134,17 @@ for(day of daysElement){
     element.target.style.fontSize = '20px'
   })
 }
+
+// Exercício 7:
+// Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+function createTask(string){
+  let taskElement = document.createElement("span")
+  let myTasksContainer = document.querySelector('.my-tasks')
+
+  taskElement.innerText = string
+  myTasksContainer.appendChild(taskElement)
+}
+
+createTask("teste")
