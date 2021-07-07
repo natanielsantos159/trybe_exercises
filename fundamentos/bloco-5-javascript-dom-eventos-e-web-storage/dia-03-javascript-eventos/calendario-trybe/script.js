73196f33e3872afd0650f14c445ac7bc6540480c
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-07 11:42:16 -0300

    exericio 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/calendario-trybe/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/calendario-trybe/script.js
@@ -1,49 +1,60 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
 
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
-    const dayListItem = document.createElement('li');
+    const dayListItem = document.createElement("li");
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-};
+  }
+}
 
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 // Exercício 1:
-// O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como 
+// O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como
 // filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
 
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
 
-function addDays(array){
-  let daysElement = document.getElementById('days')
+function addDays(array) {
+  let daysElement = document.getElementById("days");
   for (let day of array) {
-    let dayElement = document.createElement('li')
-    dayElement.className = 'day';
+    let dayElement = document.createElement("li");
+    dayElement.className = "day";
     dayElement.innerText = day;
-    switch (day){
+    switch (day) {
       case 24:
       case 31:
-        dayElement.className += ' holiday ';
+        dayElement.className += " holiday ";
         break;
       case 4:
       case 11:
       case 18:
-        dayElement.className += ' friday ';
+        dayElement.className += " friday ";
         break;
       case 25:
-        dayElement.className += ' friday holiday ';
+        dayElement.className += " friday holiday ";
     }
     daysElement.appendChild(dayElement);
   }
 }
 
-addDays(dezDaysList)
+addDays(dezDaysList);
 
 // Exercício 2:
 // Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
@@ -51,10 +62,30 @@ addDays(dezDaysList)
 // Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
 
 function createButton(string) {
-  let buttonElement = document.createElement('button')
-  let buttonsContainer = document.querySelector('.buttons-container')
-  buttonElement.id = 'btn-holiday'
-  buttonElement.innerText = string
-  buttonsContainer.appendChild(buttonElement)
+  let buttonElement = document.createElement("button");
+  let buttonsContainer = document.querySelector(".buttons-container");
+  buttonElement.id = "btn-holiday";
+  buttonElement.innerText = string;
+  buttonsContainer.appendChild(buttonElement);
 }
-createButton('Feriados')
+createButton("Feriados");
+
+// Exercício 3:
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+let buttonElement = document.getElementById("btn-holiday");
+buttonElement.addEventListener("click", clickHolidayButton);
+let holidayButtonIsOn = false;
+function clickHolidayButton(button) {
+  let holidays = document.getElementsByClassName("holiday");
+  for (let day of holidays) {
+    if (holidayButtonIsOn) {
+      // se o botão estiver com o valor true
+      day.style.backgroundColor = "rgb(238,238,238)";
+    } else {
+      // se o botão estiver com o valor false
+      day.style.backgroundColor = "violet";
+    }
+  }
+  holidayButtonIsOn = !holidayButtonIsOn // inverter valor do botao
+}
