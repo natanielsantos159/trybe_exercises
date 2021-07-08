65fcb5420c564039d3b9094cde83cd33635e2eab
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-08 12:34:12 -0300

    segundo requisito, alterar color do texto + refatoracoes

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -3,5 +3,16 @@ btnColor.addEventListener('click', function(){
   let color = document.getElementById('cor').value
   if(color !== null){
     document.body.style.backgroundColor = color
+    if(color === 'black'){
+      document.body.style.color = 'white'
+    }
+  }
+})
+
+let btnTextColor = document.getElementById('btn-text-color') 
+btnTextColor.addEventListener('click', function (){
+  let textColor = document.getElementById('text-color').value
+  if(textColor !== null){
+    document.body.style.color = textColor
   }
 })
