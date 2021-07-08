3fcc1b53e628abdce35bc9df1b7817ca5b342c45
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-08 12:42:59 -0300

    refatoracoes

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -1,18 +1,23 @@
 let btnColor = document.getElementById('btn-color') 
-btnColor.addEventListener('click', function(){
+btnColor.addEventListener('click', changeBackgroundColor)
+function changeBackgroundColor(){
   let color = document.getElementById('cor').value
   if(color !== null){
     document.body.style.backgroundColor = color
+    localStorage.setItem('background-color', color)
     if(color === 'black'){
-      document.body.style.color = 'white'
+      changeTextColor('white')
+      localStorage.setItem('background-color', 'white')
     }
   }
-})
+}
 
 let btnTextColor = document.getElementById('btn-text-color') 
-btnTextColor.addEventListener('click', function (){
+btnTextColor.addEventListener('click', changeTextColor)
+function changeTextColor(){
   let textColor = document.getElementById('text-color').value
   if(textColor !== null){
     document.body.style.color = textColor
+    localStorage.setItem('text-color', textColor)
   }
-})
+}
