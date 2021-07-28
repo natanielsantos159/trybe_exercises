60a8c6bcb1d91a81058e6446951d041ba2f94057
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-28 19:06:05 -0300

    exercicio 1

---
+++ fundamentos/bloco-8-high-order-functions/dia-01-introducao-a-high-order-functions/script.js
@@ -0,0 +1,20 @@
+/* 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. 
+Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . 
+A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com . */
+
+const getInfo = (nome) => {
+  let email = nome.toLowerCase().split(' ').join('_') + '@trybe.com'
+  return {
+    nome: nome,
+    email: email,
+  }
+}
+const newEmployees = () => {
+  const employees = {
+    id1: getInfo('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: getInfo('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: getInfo('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+newEmployees()
