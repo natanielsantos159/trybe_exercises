19be3bb3f9c1959e3d6b9155fc467428d500dfd9
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-28 19:56:08 -0300

    exercicio 3

--- fundamentos/bloco-8-high-order-functions/dia-01-introducao-a-high-order-functions/script.js
+++ fundamentos/bloco-8-high-order-functions/dia-01-introducao-a-high-order-functions/script.js
@@ -34,5 +34,31 @@ const getResult = (game, isWinner) => {
   }
 }
 
+
 const isWinner = (game, result) => game === result ? true : false
 console.log(getResult(3, isWinner))
+
+/* 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+ */
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const getResults = (rightAnswers, studentAnswers, answerChecker) => {
+  console.log(answerChecker(rightAnswers, studentAnswers))
+}
+
+getResults(RIGHT_ANSWERS, STUDENT_ANSWERS, (rightAnswers, studentAnswers)=>{ 
+  let points = 0;
+  for (index = 0; index < rightAnswers.length; index ++){
+    if(studentAnswers[index] !== 'N.A'){
+      if(studentAnswers[index] === rightAnswers[index]){
+        points += 1;
+      } else {
+        points -= 0.5;
+      }
+    }
+  }
+  return points;
+})
