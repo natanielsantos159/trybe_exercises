fa19196720b89d958d15c07d0f270675550d1c99
Nataniel Santos <nathan.santos159@hotmail.com>
2021-08-03 18:37:33 -0300

    ex2

---
+++ fundamentos/bloco-8-high-order-functions/dia-05-spread-operator-rest-parameter-destructuring-shorthand/ex2.js
@@ -0,0 +1,17 @@
+/* 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+Dica: use parâmetro rest .
+ */
+const assert = require("assert");
+
+const sum = (...args) => {
+  if (args.length !== 0) {
+    return args.reduce((acc, curr) => acc + curr);
+  }
+  return 0
+};
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
