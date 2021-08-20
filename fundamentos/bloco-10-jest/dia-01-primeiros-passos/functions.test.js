84aed1e6dd0a8bb4d22e0e6991c7fce448ceeecb
Nataniel Santos <nathan.santos159@hotmail.com>
2021-08-20 13:12:54 -0300

    exercicio 2

--- fundamentos/bloco-10-jest/dia-01-primeiros-passos/functions.test.js
+++ fundamentos/bloco-10-jest/dia-01-primeiros-passos/functions.test.js
@@ -1,4 +1,4 @@
-const { sum } = require('./functions')
+const { sum, myRemove } = require('./functions')
 
 describe('A função sum(a, b) deve retornar a soma do parâmetro a com o b', () => {
 
@@ -12,5 +12,25 @@ describe('A função sum(a, b) deve retornar a soma do parâmetro a com o b', ()
 
   it('Testa se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
     expect(() => { sum (4, '5')}).toThrowError(new Error('parameters must be numbers'))
+  })
+})
+
+describe('Testa A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array', () => {
+  const arr = [1, 2, 3, 4];
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove(arr, 3)).toEqual([1, 2, 4])
+  })
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove(arr, 3)).not.toEqual([1, 2, 3, 4])
+  })
+
+  it('Verifique se o array passado por parâmetro não sofreu alterações', () => {
+    expect(myRemove(arr, 3)).not.toEqual(arr)
+  })
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual(arr)
   })
 })
