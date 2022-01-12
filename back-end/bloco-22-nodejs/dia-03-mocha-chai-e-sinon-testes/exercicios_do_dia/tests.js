1b15e66a72f62be785bb4abad802d59ba692f83c
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-11 21:22:13 -0300

    Exercicio 1

---
+++ back-end/bloco-22-nodejs/dia-03-mocha-chai-e-sinon-testes/exercicios_do_dia/tests.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+const positivoOuNegativo = require('./positivoOuNegativo');
+
+describe("Função positivoOuNegativo", () => {
+  describe("quando recebe o valor esperado", () => {
+
+    it('deve retornar "positivo" caso receba um numero maior que 0', () => {
+      expect(positivoOuNegativo(3)).to.be.equal("positivo");
+    });
+
+    it('deve retornar "negativo" caso receba um numero menor que 0', () => {
+      expect(positivoOuNegativo(-6)).to.be.equal("negativo");
+    });
+
+    it('deve retornar "neutro" caso receba um numero igual 0', () => {
+      expect(positivoOuNegativo(0)).to.be.equal("neutro");
+    });
+  });
+});
