8074563c8bcc35a2891774f93971193430381604
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-13 19:36:13 -0300

    Desafio 4

--- back-end/bloco-22-nodejs/dia-05-express-middlewares/desafio-aula/middlewares/index.js
+++ back-end/bloco-22-nodejs/dia-05-express-middlewares/desafio-aula/middlewares/index.js
@@ -4,6 +4,7 @@ const app = express();
 const hello = require("./hello.js");
 const ping = require("./ping.js");
 const greeting = require("./greeting.js");
+const getSimpsons = require("./getSimpsons.js");
 
 app.use(express.json());
 
@@ -16,3 +17,5 @@ app.post("/hello", hello);
 app.get("/ping", ping);
 
 app.post('/greeting', greeting)
+
+app.get('/simpsons', getSimpsons)
